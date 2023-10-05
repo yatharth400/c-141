c87fc808df31177d58ef2682a2427414eb50c6bd
yatharth400 <111675597+yatharth400@users.noreply.github.com>
2023-10-05 17:56:10 +0530

    Add files via upload

---
+++ scraper.py
@@ -0,0 +1,61 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
+import time
+import pandas as pd
+
+# NASA Exoplanet URL
+START_URL = "https://exoplanets.nasa.gov/exoplanet-catalog/"
+
+# Webdriver
+browser = webdriver.Chrome("./chromedriver-win64/chromedriver.exe")
+
+browser.get(START_URL)
+
+time.sleep(10)
+
+planets_data = []
+
+# Define Exoplanet Data Scrapping Method
+def scrape():
+
+    for i in range(0,10):
+        print(f'Scrapping page {i+1} ...' )
+        soup=BeautifulSoup(browser.page_source,"html.parser")
+        for ul_tag in soup.find_all("ul",attrs={"class","exoplanet"}):
+            li_tags=ul_tag.find_all("li")
+            temp_list=[]
+            for index,li_tag in enumerate(li_tags):
+                if index==0:
+                    temp_list.append(li_tag.find_all("a")[0].contents[0])
+                else:
+                    try:
+                        temp_list.append(li_tag.contents[0])
+                    except:
+                        temp_list.append("")
+            planets_data.append(temp_list)
+        browser.find_element(by=By.XPATH, value='//*[@id="primary_column"]/footer/div/div/div/nav/span[2]/a' ).click
+
+            
+        ## ADD CODE HERE ##
+        
+
+
+
+        
+# Calling Method    
+scrape()
+
+# Define Header
+headers = ["name", "light_years_from_earth", "planet_mass", "stellar_magnitude", "discovery_date"]
+
+# Define pandas DataFrame   
+planets_df1=pd.DataFrame(planets_data,columns=headers)
+planets_df1.to_csv('scrapp_data.csv',index=True,index_label="id")
+
+
+# Convert to CSV
+
+    
+
+
